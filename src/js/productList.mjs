5a7e888564592e47533bce58700c6a59c87415c4
Charles Ofuasia <ofu23001@byui.edu>
2024-01-19 10:32:48 +0100

    pull from main

---
+++ src/js/productList.mjs
@@ -0,0 +1,14 @@
+
+
+export class ProductListing{
+    constructor(category, dataSource, listElement){
+        this.category = category;
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+
+    }
+
+    async init(){
+        const list = await this.dataSource.getData();
+    }
+}
