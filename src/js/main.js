97d886b8dbcb010d61588da9fee8ca2beace0f13
Charles Ofuasia <ofu23001@byui.edu>
2024-01-24 17:23:44 +0100

    wk3 group

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,9 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
 const productListing = new ProductList("tents", dataSource, element);
-
+loadHeaderFooter();
 productListing.init();
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -39,3 +39,28 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+
+export function renderWithTemplate(templateFn, parentElement, data, callback) {
+  
+  parentElement.insertAdjacentHTML("afterbegin", templateFn);
+  if (callback){
+    callback(data);
+  }
+}
+
+export async function loadHeaderFooter(){
+  const headerTemplate = await loadTemplate("../partials/header.html");
+const footerTemplate = await loadTemplate("../partials/footer.html");
+const headerElement = document.querySelector("#main-header");
+const footerElement = document.querySelector("#main-footer");
+renderWithTemplate(headerTemplate, headerElement);
+renderWithTemplate(footerTemplate, footerElement);
+
+}
+
+export async function loadTemplate(path){
+  const html = await fetch(path).then(convertToText);
+  const template = document.createElement("template");
+  template.innerHTML = html;
+  return template;
+}
