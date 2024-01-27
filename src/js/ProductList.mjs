a66e8b972696683d7f07b57eff2536362663f241
lucasmercau <131693290+lucasmercau@users.noreply.github.com>
2024-01-27 15:57:36 -0300

    Update ProductList

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -3,8 +3,8 @@ import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
     return `<li class="product-card">
-      <a href="product_pages/index.html?product=${product.Id}">
-        <img src="${product.Image}" alt="Image of ${product.Name}">
+      <a href="/product_pages/index.html?product=${product.Id}">
+        <img src="${product.Image || product.Images.PrimaryLarge}" alt="Image of ${product.Name}">
         <h3 class="card__brand">${product.Brand.Name}</h3>
         <h2 class="card__name">${product.Name}</h2>
         <p class="product-card__price">$${product.FinalPrice}</p>
@@ -19,8 +19,8 @@ export default class ProductList {
         this.listElement = listElement;
     }
     async init() {
-        const list = await this.dataSource.getData();
-        this.renderList(list);
+        const list = await this.dataSource.getData(this.category);
+        this.renderList(list.Result || list);
     }
     // renderList(list) {
     //     renderListWithTemplate(productCardTemplate, this.listElement, list);
@@ -31,7 +31,7 @@ export default class ProductList {
       this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
     }
     showFourTents(list) { // Stretch Activity Step 2
-        return list.filter(function(product){ return product.Id == "880RR" || product.Id == "985RF" || product.Id == "985PR" || product.Id == "344YJ"});
+        return list.filter(function(product){ return product.Id != "989CG" && product.Id != "880RT"});
     } 
 }
 
